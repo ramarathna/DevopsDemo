670171e6132778190fb04bfd424251c41df59d6f
ramu1986 <rrbc1986@gmail.com>
2020-12-31 14:17:20 +0000

    Added demo2

--- Demo2.java
+++ Demo2.java
@@ -2,7 +2,7 @@ class Demo1
 {
 public static void main(String args[])
 {
-System.out.println("Java demo class 1");
+System.out.println("Java demo class 2");
 System.out.println("This is second line from Demo class");
 }
 
