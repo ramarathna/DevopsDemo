cc556cc843678233176b677fd6022f59e236352a
ramarathna <ramarathna.chikkanna@gmail.com>
2020-12-31 16:02:17 +0100

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -3,7 +3,8 @@ class Demo1
 public static void main(String args[])
 {
 System.out.println("Java demo class 2");
-System.out.println("This is second line from Demo class");
+System.out.println("This is second line from Demo2 class");
+  System.out.println("Remote");
 }
 
 }
